00291f527cb296b0743cdda6afc95f18f13fe1fa
AndyPicket <andypicket0@gmail.com>
2021-03-14 17:15:48 +0100

    added supervisor

--- install.sh
+++ install.sh
@@ -42,12 +42,14 @@ if [ -f /etc/redhat-release ]; then
      ./install_supervisord.sh
 fi
 
+
 if [ -f /etc/debian_version ]; then
     apt-get update && apt-get upgrade -y
     apt-get install -y python-pip
     pip install --upgrade pip
     apt-get install apt-transport-https -y
     apt-get install build-essential -y #needed for building some python modules
+    ./install_supervisord.sh
 fi
 echo "[`date`] Geting configuration"
 
